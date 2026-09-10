98720e3c6e3819e3efd4b092b8078f550f893f73
Syed Bilal Alam <107833932+syedbilalalam@users.noreply.github.com>
2026-09-10 12:15:34 +0500

    feat: add user input

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,22 @@
 int main()
 {
     Rect rectangle(Dimension(10, 10));
-    rectangle.Draw();
+    int height, width;
 
+    // Taking dimension from user:
+    std::cout<<"Enter height of the rectangle: ";
+    std::cin>>height;
+    std::cout<<"Enter width of the rectangle: ";
+    std::cin>>width;
+    
+    // Updating dimensions
+    rectangle.setDimension(Dimension(height, width));
+    
+    // Drawing rectangle
+    rectangle.Draw();
     std::cout << std::endl;
+
+    // Rectangle info
     std::cout << "Rectangle height: " << rectangle.dimension()->height() << std::endl;
     std::cout << "Rectangle width: " << rectangle.dimension()->width() << std::endl;
 
